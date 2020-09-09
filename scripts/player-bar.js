43d89e14eea5204a2e024f6d4caa2853a9ee5fcf
Jenice Kho <jenkho9@gmail.com>
2020-09-08 19:56:51 -0500

    updated player-bar

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -14,4 +14,15 @@
         const nextSong = album.songs[nextSongIndex];
         player.playPause(nextSong);
     });
+
+    $("button#previous").on("click", (event) => {
+        if (player.playState !== "playing") { return; }
+
+        currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const prevSongIndex = currentSongIndex - 1;
+        if(prevSongIndex >= album.songs.length) { return; }
+
+        const prevSong = album.songs[prevSongIndex];
+        player.playPause(prevSong);
+    });
 }
