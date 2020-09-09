365d206c98e2ac30f46aa499e0bb609c4e97ff58
Jenice Kho <jenkho9@gmail.com>
2020-09-08 21:52:15 -0500

    jquery sliders checkpoint complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,5 +1,5 @@
 {
-    $("button#play-pause").on("click", (event) => {
+    $("button#play-pause").on("click", function() {
         player.playPause();
         $(this).attr("playState", player.playState);
     });
@@ -14,4 +14,29 @@
         const nextSong = album.songs[nextSongIndex];
         player.playPause(nextSong);
     });
+
+    $("button#previous").on("click", (event) => {
+        if (player.playState !== "playing") { return; }
+
+        currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const prevSongIndex = currentSongIndex - 1;
+        if(prevSongIndex >= album.songs.length) { return; }
+
+        const prevSong = album.songs[prevSongIndex];
+        player.playPause(prevSong);
+    });
+
+    $("#time-control input").on("input", (event) => {
+        player.skipTo(event.target.value);
+    });
+
+     //percent = (part/whole) * 100
+    setInterval((event) => {
+        if (player.playState !== "playing") { return; }
+        const currentTime = player.getTime();
+        const duration = player.getDuration();
+        const percent = (currentTime/duration) * 100;
+        $("#time-control .current-time").text(currentTime);
+        $("#time-control input").val(percent);
+    }, 1000);
 }
